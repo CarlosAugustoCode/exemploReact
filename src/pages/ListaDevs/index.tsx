e5239a7da9e68ad8d6e4acec740ce860bda028f5
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2023-08-17 21:40:41 -0300

    Prática consumindo API na Lista de Serviços

--- src/pages/ListaDevs/index.tsx
+++ src/pages/ListaDevs/index.tsx
@@ -1,97 +1,80 @@
-import { useState } from "react"
+
+import { useEffect, useState } from "react"
 import CardDev from "../../components/CardDev"
 import "./style.css"
-// import imgClientes from "../../assets/img/clientes.png"
-// import imgMaos from "../../assets/img/maos_dev.png"
-// import imgAlfinetes from "../../assets/img/alfinete.png"
+
+import api from "../../utils/api";
 
 export default function ListaDevs() {
+    const [devs, setDevs] = useState<any[]>([]);
 
-    const [devs, setDevs] = useState<any[]>([
-        {
-            img_perfil: "https://github.com/Thiago-Nascimento.png",
-            nome: "Thiago Nascimento",
-            email: "thiago@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/JessicaSanto.png",
-            nome: "Jessica Franzon",
-            email: "jessica@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/odirlei-assis.png",
-            nome: "Odirlei Sabella",
-            email: "odirlei@email.com",
-            skills: ["HTML", "CSS", "ANGULAR"]
-        },
-        {
-            img_perfil: "https://github.com/alexiamelhado18.png",
-            nome: "Aléxia Vitória",
-            email: "alexia@email.com",
-            skills: ["PYTHON", "VUE", "REACT"]
-        }
-    ]);
 
     const [skillDigitada, setSkillDigitada] = useState<string>("");
-    const [listaDevsFiltrados, setListaDevFiltrados] = useState<any[]>(devs);
 
-    function buscarporSkill(event: any){
+    const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs);
+
+    useEffect(()=>{
+        document.title = "Lista de Dev's | VSConnect"
+
+        listarDesenvolvedores();
+    }, [] )
+
+    function buscarPorSkill(event: any){
         event.preventDefault();
 
-        const devsFiltrados = devs.filter((dev: any) => dev.skills.includes(skillDigitada.toLocaleUpperCase()));
+        const devsFiltrados = devs.filter((dev: any) => dev.hardSkills.includes(skillDigitada.toLocaleUpperCase()));
 
-        if(devsFiltrados.length === 0) {
-            alert("Nenhum desenvolvedor(a) com essa skill")
+        if(devsFiltrados.length === 0){
+            return alert("Nenhum desenvolvedor foi encontrado com essa Skill")
         }else{
-            setListaDevFiltrados(devsFiltrados)
+            setListaDevsFiltrados(devsFiltrados)
         }
     }
 
     function retornoDevsGeral(event: any){
         if(event.target.value === ""){
-            setListaDevFiltrados(devs)
-        }
+    listarDesenvolvedores()        }
         setSkillDigitada(event.target.value)
     }
 
+    function listarDesenvolvedores(){
+        api.get("users").then((response: any) => {
+            console.log(response.data)
+            setDevs(response.data)
+        })
+    }
     return (
-
         <main id="lista-devs">
             <div className="container container_lista_devs">
                 <div className="lista_devs_conteudo">
                     <h1>Lista de Devs</h1>
-                    <hr />
-                    <form method="post" onSubmit={buscarporSkill}>
-                        <div className="wrapper_form">
-                            <label htmlFor="busca">Procurar desenvolvedores</label>
-                            <div className="campo-label">
-                                <input type="search" name="campo-busca" id="busca" placeholder="Buscar desenvolvedores por tecnologias..." onChange={retornoDevsGeral} />
-                                <button type="submit">Buscar</button>
+                    <hr/>
+                        <form method="post" onSubmit={buscarPorSkill}>
+                            <div className="wrapper_form">
+                                <label htmlFor="busca">Procurar desenvolvedores</label>
+                                <div className="campo-label">
+                                    <input type="search" name="campo-busca" id="busca" placeholder="Buscar desenvolvedores por tecnologias..." onChange={retornoDevsGeral}/>
+                                        <button type="submit">Buscar</button>
+                                </div>
                             </div>
+                        </form>
+                        <div className="wrapper_lista">
+                            <ul>
+                                {devs.map((dev: any, index: number) => {
+                                    return <li key={index}>
+                                        <CardDev
+                                            foto={dev.user_img}
+                                            nome={dev.nome}
+                                            email={dev.email}
+                                            techs={dev.hardSkills}
+                                        />
+                                    </li>
+                                }
+                                )}
+                            </ul>
                         </div>
-                    </form>
-                    <div className="wrapper_lista">
-                        <ul>
-                            {listaDevsFiltrados.map((dev: any, index: number) => {
-                                return <li>
-                                    <CardDev 
-                                    foto = {dev.img_perfil}
-                                    nome = {dev.nome}
-                                    email = {dev.email}
-                                    techs = {dev.skills}
-                                    />
-                                </li>
-                            })}
-                            
-                        </ul>
-                    </div>
                 </div>
             </div>
         </main>
-
-
     )
-
 }
